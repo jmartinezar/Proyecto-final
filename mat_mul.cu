e00cb897325bee04d77386fed84e9c11653b1ffb
jmartinezar <jmartinezar@unal.edu.co>
2024-07-19 18:11:38 -0500

    size has been further reduced

--- mat_mul.cu
+++ mat_mul.cu
@@ -21,7 +21,7 @@ __global__ void matrixMul(const double *A, const double *B, double *C, int width
 
 int main(void) {
     // Size of the matrices (assuming square matrices for simplicity)
-    int width = 1000000;
+    int width = 1000;
     size_t size = width * width * sizeof(double);
 
     // Allocate memory for host matrices
