91cd71c8cdb8e62e5d1409270e1c6e95af1244b5
CosmeticMichu <eariasp@unal.edu.co>
2024-07-25 15:42:40 -0400

    automatized execution with make
    data is generated using a bash 'for' loop where the output is correctly printed to data/.txt and logs/.log files according to the output type (stdout -> .txt, stderr -> .log). A Python file is as well provided in order to plot data to figures in figs/ directory

--- Makefile
+++ Makefile
@@ -1,14 +1,38 @@
-all: mm
+COMPILER=nvcc
+DAT=data
+LOG=logs
+SIZES=10 100 500 1000 5000 10000 15000 20000 25000 29000
+
+header=size\ttime(s)
+
+all: data
+
+matmul.x: mat_mul.cu
+	@echo "\n\033[1;38;5mCompiling $< \033[0m"
+	$(COMPILER) $< -o $@
+
+vector.x: vector.cu
+	@echo "\n\033[1;38;5mCompiling $< \033[0m"
+	$(COMPILER) $< -o $@
+
+matmul: matmul.x
 	./$<
 
-mm: mat_mul.cu
-	nvcc $< -o $@
-
-vec: vector
+vector: vector.x
 	./$<
 
-vector: vector.cu
-	nvcc $< -o $@
+vector-times: vector.x
+	@echo "     \033[1;38;5;214mVECTOR\033[0m\n$(header)" && for size in $(SIZES); do ./$< $$size 2>$(LOG)/$@.log; done | tee $(DAT)/$@.txt
+
+matmul-times: matmul.x
+	@echo "     \033[1;38;5;214mMATMUL\033[0m\n$(header)" && for size in $(SIZES); do ./$< $$size 2>$(LOG)/$@.log; done | tee $(DAT)/$@.txt
+
+data: vector-times matmul-times
+
+# el comando no se incluye en el target all porque el server remoto original de trabajo
+# no tiene matplotlib instalado. Se deja como opción para graficar en una máquina con la libreria
+plot: plot.py data
+	python $<
 
 clean:
-	rm mm vector
+	rm data/* logs/* *.x
---
+++ plot.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# --------------------- Read data ----------------------
+data_vector = np.loadtxt('data/vector-times.txt')
+data_matmul = np.loadtxt('data/matmul-times.txt')
+
+# ---------------------- Plotting ----------------------
+# matmul
+nologcolor = '#1791E0'
+logcolor = '#B30000'
+
+fig, ax = plt.subplots(sharex = True, figsize = (6,5))
+fig.suptitle("GPU matrix multiplication time scaling using CUDA", fontweight="bold", fontsize=13)
+fig.supxlabel("Matrix size", size=13)
+
+ax.set_ylabel('GPU wall time [s]', size=13, color = nologcolor)
+# ax.set_xscale('log')
+ax.plot(data_matmul[:,0], data_matmul[:,1], marker = '*', ms = 9, mfc = 'none', mew = 1.0, lw = 1.2, c = nologcolor)
+
+ax2 = ax.twinx()
+ax2.set_ylabel('GPU wall time [s](logscale)', size=13, color = logcolor)
+ax2.set_yscale('log')
+# ax2.set_xscale('log')
+ax2.plot(data_matmul[:,0], data_matmul[:,1], marker = 'o', mfc = 'none', mew = 1.0, ms = 7.5, lw = 1.2, c = logcolor)
+plt.tight_layout()
+plt.savefig("figs/matmul-weak.pdf")
+
+# vector
+fig, ax = plt.subplots(sharey = True, figsize = (6,5))
+fig.suptitle("GPU matrix multiplication time scaling using CUDA", fontweight="bold", fontsize=13)
+fig.supylabel("GPU wall time [s]", size=13)
+
+ax.set_xlabel('Vector size', size=13, color = '#2a088e')
+ax.plot(data_vector[:,0], data_vector[:,1], marker = '*', ms = 8.5, color = '#2a088e')
+
+ax2 = ax.twiny()
+ax2.set_xlabel('Vector size (logscale)', size=13, color = '#f4a40a')
+ax2.set_xscale('log')
+ax2.plot(data_vector[:,0], data_vector[:,1], marker = 'o', color = '#f4a40a')
+plt.tight_layout()
+plt.savefig("figs/vector-weak.pdf")
+
+plt.show() # plot both at once
