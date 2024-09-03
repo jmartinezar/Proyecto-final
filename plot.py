f8a9af0fe826dbe2acf7e498ffcba469fa27c19c
tprieto <tprieto@unal.edu.co>
2024-09-02 22:14:12 -0500

    present data

--- plot.py
+++ plot.py
@@ -7,21 +7,24 @@ TYPE = sys.argv[1].strip().lower()
 data_vector = np.loadtxt(f'data/{TYPE}_vector-times.txt')
 data_matmul = np.loadtxt(f'data/{TYPE}_matmul-times.txt')
 
+upperType = "GPU" if TYPE == "gpu" else "CPU"
+library = "CUDA" if TYPE == "gpu" else "EIGEN/DENSE"
+
 # ---------------------- Plotting ----------------------
 # matmul
 nologcolor = '#1791E0'
-logcolor = '#B30000'
+logcolor = '#B30000'    
 
 fig, ax = plt.subplots(sharex = True, figsize = (6,5))
-fig.suptitle("GPU matrix multiplication time scaling using CUDA", fontweight="bold", fontsize=13)
+fig.suptitle(f"{upperType} matrix multiplication time scaling using {library}", fontweight="bold", fontsize=13)
 fig.supxlabel("Matrix size", size=13)
 
-ax.set_ylabel('GPU wall time [s]', size=13, color = nologcolor)
+ax.set_ylabel(f'{upperType} wall time [s]', size=13, color = nologcolor)
 # ax.set_xscale('log')
 ax.plot(data_matmul[:,0], data_matmul[:,1], marker = '*', ms = 9, mfc = 'none', mew = 1.0, lw = 1.2, c = nologcolor)
 
 ax2 = ax.twinx()
-ax2.set_ylabel('GPU wall time [s](logscale)', size=13, color = logcolor)
+ax2.set_ylabel(f'{upperType} wall time [s](logscale)', size=13, color = logcolor)
 ax2.set_yscale('log')
 # ax2.set_xscale('log')
 ax2.plot(data_matmul[:,0], data_matmul[:,1], marker = 'o', mfc = 'none', mew = 1.0, ms = 7.5, lw = 1.2, c = logcolor)
@@ -30,8 +33,8 @@ plt.savefig("figs/matmul-strong.pdf")
 
 # vector
 # fig, ax = plt.subplots(sharey = True, figsize = (6,5))
-# fig.suptitle("GPU matrix multiplication time scaling using CUDA", fontweight="bold", fontsize=13)
-# fig.supylabel("GPU wall time [s]", size=13)
+# fig.suptitle(f"{upperType} matrix multiplication time scaling using {library}", fontweight="bold", fontsize=13)
+# fig.supylabel(f"{upperType} wall time [s]", size=13)
 
 # ax.set_xlabel('Vector size', size=13, color = '#2a088e')
 # ax.plot(data_vector[:,0], data_vector[:,1], marker = '*', ms = 8.5, color = '#2a088e')
