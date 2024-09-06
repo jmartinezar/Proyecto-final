f58579d9de66e5dcf901649495b3ab94c7b85068
CosmeticMichu <eariasp@unal.edu.co>
2024-09-06 14:52:14 -0400

    improved parallelization in cpu

--- CPU/mat_mul.cpp
+++ CPU/mat_mul.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <chrono>
 #include <Eigen/Dense>
-#include <omp.h> // Include OpenMP header
+// #include <omp.h> // Include OpenMP header
 
 int main(int argc, char *argv[])
 {
@@ -13,26 +13,11 @@ int main(int argc, char *argv[])
     int matrix_B_width = matrix_size;
     int matrix_B_height = matrix_size;
 
-    // Crear matrices usando Eigen (parallelized initialization)
-    Eigen::MatrixXd matrix_A(matrix_A_height, matrix_A_width);
-    Eigen::MatrixXd matrix_B(matrix_B_height, matrix_B_width);
+    // Crear matrices usando Eigen
+    Eigen::MatrixXd matrix_A = Eigen::MatrixXd::Constant(matrix_A_height, matrix_A_width, 1.0); // Matriz A con valores 1.0
+    Eigen::MatrixXd matrix_B = Eigen::MatrixXd::Constant(matrix_B_height, matrix_B_width, 2.0); // Matriz B con valores 2.0
     Eigen::MatrixXd result_matrix(matrix_A_height, matrix_B_width); // Matriz C para almacenar el resultado
-
-    // Parallel initialization of matrix_A and matrix_B
-    #pragma omp parallel for collapse(2)
-    for (int i = 0; i < matrix_A_height; i++) {
-        for (int j = 0; j < matrix_A_width; j++) {
-            matrix_A(i, j) = 1.0; // Fill matrix_A with 1.0
-        }
-    }
-
-    #pragma omp parallel for collapse(2)
-    for (int i = 0; i < matrix_B_height; i++) {
-        for (int j = 0; j < matrix_B_width; j++) {
-            matrix_B(i, j) = 2.0; // Fill matrix_B with 2.0
-        }
-    }
-
+    
     // Medir el tiempo de ejecución
     auto start_time = std::chrono::system_clock::now(); // inicio del tiempo
 
